c84a1bf0634db0f55589f426e9e8593e90b40def
Matteo <mvalorianigmail.com>
2016-03-01 22:01:18 +0100

    Creazione sample Xamarin

---
+++ Sample.Xamarin.Form/Sample.Xamarin.Form.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Sample.Xamarin.Form.WinPhone.Resources;
+
+namespace Sample.Xamarin.Form.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
